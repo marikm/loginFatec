bc3b045bcda83cde984e9825b82830b48807a31a
unknown <matsumotomarina3@gmail.com>
2023-11-03 17:59:58 -0300

    divs principais estabelecidas no login html

--- login.php
+++ login.php
@@ -7,7 +7,25 @@
 </head>
 <body>
     <form action="">
-        
+       <h1>Sejam Bem-Vindos</h1> 
+       <main> <!-- Dentro dessa tag está o conteúdo         principal da pagina-->
+            <div class="login">
+                <div class="imagem"><img src="./img/perfil.png" alt="imagem de login"></div>
+                <form action="">
+                    <div>
+                        <label for="usuario">USUÁRIO</label>
+                        <input type="text" name="usuario" id="usuario"/>
+                    </div>
+                    <div>
+                        <label for="senha">SENHA</label>
+                        <input type="password" name="senha" id="senha"/>
+                    </div>
+                    <div>
+                        <input type="submit" value="L O G I N">
+                    </div>
+                </form>
+            </div>
+       </main>
     </form>
     
 </body>
